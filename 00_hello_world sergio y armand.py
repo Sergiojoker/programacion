d3c46f74b3bbdc9d0150b12ca44d9849746256e5
Sergiojoker <147618287+Sergiojoker@users.noreply.github.com>
2023-10-24 07:20:56 -0500

    Add files via upload

---
+++ 00_hello_world sergio y armand.py
@@ -0,0 +1,14 @@
+# Primer Programa que escribes en un nuevo lenguaje
+print("Hello, world!")
+print("*********************************************************************************************************************************************************************************************************************************")
+# Nombre De Los Participantes
+print("Sergio Esteban Pacheco, Armand Daniel Ceballos")
+# Fecha
+print("Fecha:18 de Octubre Del 2023")
+# Ficha
+print(" Ficha:2749613")
+# Programa De Fomacion
+print(" Formacion:Diseño e Integracion de Automatismos Mecatonicos")
+print("*********************************************************************************************************************************************************************************************************************************")
+print("El texto  en pantalla sirve de interaccion con el usuario de un programa es prescindible sin embargo una adecuda interfaz permite,")
+print("sin embargo una adecuada interfaz permite claridad en la ejecucion.")
---
+++ 01_variables_datos sergio y armand.py
@@ -0,0 +1,10 @@
+#colocar las variantes
+Nombre="Sergio"
+Edad=18
+Altura=1,75
+Aprendiz="true"
+# mostrar las variantes
+print("La variante 'nombre' es:", type(Nombre))
+print("la variante 'edad' es:", type(Edad))
+print("la variante 'altura' es:", type(Altura))
+print("la variante 'Aprendiz' es:", type(Aprendiz))
